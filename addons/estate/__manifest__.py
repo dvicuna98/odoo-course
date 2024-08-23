3c6cb8ec3824c1a6b1da5eecac3e5ce891c0bafc
Diego Vicuna <dvicuna@casagrande.edu.ec>
2024-08-23 15:50:46 -0500

    feature: new module added
    - model, view, security added

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Diego Vicuna",
+    'category': "App",
+    'application': True,
+    'description': "This module doesnt do anything",
+    'data' : [
+        'security/ir.model.access.csv',
+        'views/menu.xml'
+    ]
+}
